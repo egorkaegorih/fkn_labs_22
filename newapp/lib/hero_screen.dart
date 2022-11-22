28efbdd5f7a8847f89f68454cb78cd138bcea9b3
egorkaegorih <egorkaegorih@gmail.com>
2022-11-22 22:34:22 +0600

    add change in lab2

--- newapp/lib/hero_screen.dart
+++ newapp/lib/hero_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'heroes.dart';
 
+import 'package:cached_network_image/cached_network_image.dart';
+
 class HeroScreen extends StatelessWidget {
   final Heroes _hero;
 
@@ -12,52 +14,51 @@ class HeroScreen extends StatelessWidget {
     return Hero(
         tag: _hero.name,
         child: Scaffold(
-          body: Stack(
-            fit: StackFit.expand,
-            children: [
-              Image.network(
-                _hero.pathImage,
-                fit: BoxFit.cover,
+            body: Stack(
+          fit: StackFit.expand,
+          children: [
+            CachedNetworkImage(
+              imageUrl: _hero.pathImage,
+              fit: BoxFit.cover,
+              placeholder: (context, url) => const CircularProgressIndicator(),
+              errorWidget: (context, url, error) => const Icon(
+                Icons.error,
+                color: Colors.red,
               ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                      padding:
-                          EdgeInsets.symmetric(vertical: 15, horizontal: 10),
-                      child: BackButton(
-                        color: Colors.white,
-                      )),
-                  Expanded(child: Container()),
-                  Padding(
-                      padding:
-                          EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Padding(
-                            padding: EdgeInsets.symmetric(vertical: 5),
-                            child: Text(
-                              _hero.name,
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 25,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                          Text(
-                            _hero.info,
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold),
-                          )
-                        ],
-                      ))
-                ],
-              )
-            ],
-          ),
-        ));
+            ),
+            const Positioned(
+                top: 35,
+                left: 10,
+                child: BackButton(
+                  color: Colors.white,
+                )),
+            Positioned(
+                bottom: 10,
+                left: 10,
+                right: 0,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 5),
+                      child: Text(
+                        _hero.name,
+                        style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Text(
+                      _hero.info,
+                      style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ))
+          ],
+        )));
   }
 }
--- newapp/lib/heroes_card.dart
+++ newapp/lib/heroes_card.dart
@@ -1,6 +1,6 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:newapp/heroes.dart';
-import 'package:newapp/main.dart';
+import 'heroes.dart';
 
 class HeroesCard extends StatelessWidget {
   final Heroes _heroes;
@@ -17,28 +17,27 @@ class HeroesCard extends StatelessWidget {
             margin: const EdgeInsets.all(15),
             shape:
                 RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-            child: Container(
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: NetworkImage(_heroes.pathImage),
-                      fit: BoxFit.fitHeight),
+            child: Stack(fit: StackFit.expand, children: [
+              CachedNetworkImage(
+                imageUrl: _heroes.pathImage,
+                fit: BoxFit.cover,
+                placeholder: (context, url) =>
+                    const CircularProgressIndicator(),
+                errorWidget: (context, url, error) => const Icon(
+                  Icons.error,
+                  color: Colors.red,
                 ),
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Expanded(
-                        child: Container(),
-                      ),
-                      Padding(
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 25, vertical: 40),
-                        child: Text(
-                            style: const TextStyle(
-                                color: Colors.white,
-                                fontSize: 30,
-                                fontWeight: FontWeight.bold),
-                            _heroes.name),
-                      )
-                    ]))));
+              ),
+              Positioned(
+                bottom: 10,
+                left: 10,
+                child: Text(
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold),
+                    _heroes.name),
+              )
+            ])));
   }
 }
